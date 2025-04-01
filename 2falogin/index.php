515608b2cbd865396a7334d7365fb0938ae508aa
TCRMilanvd <9023420@student.zadkine.nl>
2025-04-01 09:07:29 +0200

    2faLogin

---
+++ 2falogin/index.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+if (!isset($_SESSION['user_id'])) {
+    header('Location: login.php');
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+</head>
+<body>
+    <center>
+        <h1></h1>Dashboard</h1>
+        <p>Welkom, Gebruiker!</p>
+        <p>(Ik wou ervoor zorgen dat het de gebruikersnaam liet zien, maar werkte niet :()</p>
+        <p><a href="logout.php">Uitloggen</a></p>
+        <p><a href="register.php">Registreren</a></p>
+        <p><a href="login.php">Login</a></p>
+    </center>
+</body>
+</html>
